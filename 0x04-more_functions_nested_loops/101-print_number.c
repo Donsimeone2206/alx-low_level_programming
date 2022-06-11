ee120e959f2ed7fa6e16e67c6536d49cddaf1b0a
Simeon Mustapha <simeon.mustapha@gmail.com>
2022-06-10 18:12:06 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_number - a function print a number.
+ * @n : the number
+ *
+ */
+
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+}
