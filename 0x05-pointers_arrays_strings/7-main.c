b8d2c79179a4238814dbce217588e22c6256b095
Simeon Mustapha <simeon.mustapha@gmail.com>
2022-06-14 17:06:25 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str, *str1;
+
+	str = "0123456789";
+	str1 = "Holberton!";
+	puts_half(str);
+	puts_half(str1);
+	return (0);
+}
